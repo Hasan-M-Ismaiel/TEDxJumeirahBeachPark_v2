0749136cbd90b126e353fe366f79a7a454ba2910
Hasan <hasanismaiel19951995@gmail.com>
2026-05-09 18:44:05 +0300

    speakers

--- resources/views/includes/speakers.blade.php
+++ resources/views/includes/speakers.blade.php
@@ -30,7 +30,10 @@
                                     <span><strong>{{$speaker->title}}</strong></span>
                                     @if ($event->type == "Standard Event")
                                     <span style="color: black;"><strong>{{$speaker->topic_title}}</strong></span><br>
-                                    <a href="{{ $speaker->talk }}" style="TEXT-DECORATION: underline; color:red" target="_blank"> Watch on TEDx YouTube</a>
+                                     @if ($event->title == "Upcoming Standard Event: Look Within")
+                                     @else
+                                     <a href="{{ $speaker->talk }}" style="TEXT-DECORATION: underline; color:red" target="_blank"> Watch on TEDx YouTube</a>
+                                     @endif
                                     @else
                                     @endif
                                 </div>
